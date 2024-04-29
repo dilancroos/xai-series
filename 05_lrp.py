599bd771fb148517aca50a39e41191893557c97b
dilancroos <dilancroos@outlook.com>
2024-04-29 18:55:19 +0200

    05 Run: W results

--- 05_lrp.py
+++ 05_lrp.py
@@ -1,4 +1,5 @@
 # %% Imports
+import pandas as pd
 import torch
 import torch.nn as nn
 import torchvision
@@ -8,11 +9,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch.optim as optim
 import copy
-import pandas as pd 
+import pandas as pd
 
+# %%
 # Set GPU device
 print(torch.cuda.is_available())
-device = torch.device("cuda:0")
+print(torch.backends.mps.is_available())
+device = torch.device('mps')
+device
+# device = torch.device("cuda:0")
 
 
 # %% Load data
@@ -26,15 +31,16 @@ test_dataset = torchvision.datasets.ImageFolder(root=TRAIN_ROOT)
 class CNNModel(nn.Module):
     def __init__(self):
         super(CNNModel, self).__init__()
-        self.vgg16 = models.vgg16(pretrained=True) 
+        self.vgg16 = models.vgg16(pretrained=True)
 
         # Replace output layer according to our problem
-        in_feats = self.vgg16.classifier[6].in_features 
+        in_feats = self.vgg16.classifier[6].in_features
         self.vgg16.classifier[6] = nn.Linear(in_feats, 4)
 
     def forward(self, x):
         x = self.vgg16(x)
         return x
+
 
 model = CNNModel()
 model.to(device)
@@ -42,22 +48,22 @@ model
 
 # %% Prepare data for pretrained model
 train_dataset = torchvision.datasets.ImageFolder(
-        root=TRAIN_ROOT,
-        transform=transforms.Compose([
-                      transforms.Resize((255,255)),
-                      transforms.ToTensor()
-        ])
+    root=TRAIN_ROOT,
+    transform=transforms.Compose([
+        transforms.Resize((255, 255)),
+        transforms.ToTensor()
+    ])
 )
 
 test_dataset = torchvision.datasets.ImageFolder(
-        root=TEST_ROOT,
-        transform=transforms.Compose([
-                      transforms.Resize((255,255)),
-                      transforms.ToTensor()
-        ])
+    root=TEST_ROOT,
+    transform=transforms.Compose([
+        transforms.Resize((255, 255)),
+        transforms.ToTensor()
+    ])
 )
 
-#train_dataset[0][0].permute(1,2,0)
+# train_dataset[0][0].permute(1,2,0)
 
 # %% Create data loaders
 batch_size = 32
@@ -78,7 +84,7 @@ optimizer = optim.Adam(model.parameters(), lr=0.00001)
 epochs = 10
 
 # Iterate x epochs over the train data
-for epoch in range(epochs):  
+for epoch in range(epochs):
     for i, batch in enumerate(train_loader, 0):
         inputs, labels = batch
         inputs = inputs.to(device)
@@ -90,15 +96,15 @@ for epoch in range(epochs):
         loss.backward()
         optimizer.step()
         print(loss)
+    print("Epoch ", epoch, " done.")
 
 # %% Inspect predictions for first batch
-import pandas as pd
 inputs, labels = next(iter(test_loader))
 inputs = inputs.to(device)
 labels = labels.numpy()
 outputs = model(inputs).max(1).indices.detach().cpu().numpy()
 comparison = pd.DataFrame()
-print("Batch accuracy: ", (labels==outputs).sum()/len(labels))
+print("Batch accuracy: ", (labels == outputs).sum()/len(labels))
 comparison["labels"] = labels
 
 comparison["outputs"] = outputs
@@ -109,55 +115,65 @@ comparison
 # Source: https://git.tu-berlin.de/gmontavon/lrp-tutorial
 # http://iphome.hhi.de/samek/pdf/MonXAI19.pdf
 
+
 def new_layer(layer, g):
     """Clone a layer and pass its parameters through the function g."""
     layer = copy.deepcopy(layer)
-    try: layer.weight = torch.nn.Parameter(g(layer.weight))
-    except AttributeError: pass
-    try: layer.bias = torch.nn.Parameter(g(layer.bias))
-    except AttributeError: pass
+    try:
+        layer.weight = torch.nn.Parameter(g(layer.weight))
+    except AttributeError:
+        pass
+    try:
+        layer.bias = torch.nn.Parameter(g(layer.bias))
+    except AttributeError:
+        pass
     return layer
+
 
 def dense_to_conv(layers):
     """ Converts a dense layer to a conv layer """
     newlayers = []
-    for i,layer in enumerate(layers):
+    for i, layer in enumerate(layers):
         if isinstance(layer, nn.Linear):
             newlayer = None
             if i == 0:
                 m, n = 512, layer.weight.shape[0]
-                newlayer = nn.Conv2d(m,n,7)
-                newlayer.weight = nn.Parameter(layer.weight.reshape(n,m,7,7))
+                newlayer = nn.Conv2d(m, n, 7)
+                newlayer.weight = nn.Parameter(
+                    layer.weight.reshape(n, m, 7, 7))
             else:
-                m,n = layer.weight.shape[1],layer.weight.shape[0]
-                newlayer = nn.Conv2d(m,n,1)
-                newlayer.weight = nn.Parameter(layer.weight.reshape(n,m,1,1))
+                m, n = layer.weight.shape[1], layer.weight.shape[0]
+                newlayer = nn.Conv2d(m, n, 1)
+                newlayer.weight = nn.Parameter(
+                    layer.weight.reshape(n, m, 1, 1))
             newlayer.bias = nn.Parameter(layer.bias)
             newlayers += [newlayer]
         else:
             newlayers += [layer]
     return newlayers
 
+
 def get_linear_layer_indices(model):
     offset = len(model.vgg16._modules['features']) + 1
     indices = []
-    for i, layer in enumerate(model.vgg16._modules['classifier']): 
-        if isinstance(layer, nn.Linear): 
+    for i, layer in enumerate(model.vgg16._modules['classifier']):
+        if isinstance(layer, nn.Linear):
             indices.append(i)
     indices = [offset + val for val in indices]
     return indices
+
 
 def apply_lrp_on_vgg16(model, image):
     image = torch.unsqueeze(image, 0)
     # >>> Step 1: Extract layers
     layers = list(model.vgg16._modules['features']) \
-                + [model.vgg16._modules['avgpool']] \
-                + dense_to_conv(list(model.vgg16._modules['classifier']))
+        + [model.vgg16._modules['avgpool']] \
+        + dense_to_conv(list(model.vgg16._modules['classifier']))
     linear_layer_indices = get_linear_layer_indices(model)
     # >>> Step 2: Propagate image through layers and store activations
     n_layers = len(layers)
-    activations = [image] + [None] * n_layers # list of activations
-    
+    activations = [image] + [None] * n_layers  # list of activations
+
     for layer in range(n_layers):
         if layer in linear_layer_indices:
             if layer == 32:
@@ -170,8 +186,8 @@ def apply_lrp_on_vgg16(model, image):
     # >>> Step 3: Replace last layer with one-hot-encoding
     output_activation = activations[-1].detach().cpu().numpy()
     max_activation = output_activation.max()
-    one_hot_output = [val if val == max_activation else 0 
-                        for val in output_activation[0]]
+    one_hot_output = [val if val == max_activation else 0
+                      for val in output_activation[0]]
 
     activations[-1] = torch.FloatTensor([one_hot_output]).to(device)
 
@@ -188,52 +204,60 @@ def apply_lrp_on_vgg16(model, image):
            isinstance(current, torch.nn.AvgPool2d) or\
            isinstance(current, torch.nn.Linear):
             activations[layer] = activations[layer].data.requires_grad_(True)
-            
+
             # Apply variants of LRP depending on the depth
             # see: https://link.springer.com/chapter/10.1007%2F978-3-030-28954-6_10
             # Lower layers, LRP-gamma >> Favor positive contributions (activations)
-            if layer <= 16:       rho = lambda p: p + 0.25*p.clamp(min=0); incr = lambda z: z+1e-9
+            if layer <= 16:
+                def rho(p): return p + 0.25*p.clamp(min=0)
+                def incr(z): return z+1e-9
             # Middle layers, LRP-epsilon >> Remove some noise / Only most salient factors survive
-            if 17 <= layer <= 30: rho = lambda p: p;                       incr = lambda z: z+1e-9+0.25*((z**2).mean()**.5).data
+            if 17 <= layer <= 30:
+                def rho(p): return p
+                def incr(z): return z+1e-9+0.25*((z**2).mean()**.5).data
             # Upper Layers, LRP-0 >> Basic rule
-            if layer >= 31:       rho = lambda p: p;                       incr = lambda z: z+1e-9
-            
+            if layer >= 31:
+                def rho(p): return p
+                def incr(z): return z+1e-9
+
             # Transform weights of layer and execute forward pass
-            z = incr(new_layer(layers[layer],rho).forward(activations[layer]))
+            z = incr(new_layer(layers[layer], rho).forward(activations[layer]))
             # Element-wise division between relevance of the next layer and z
-            s = (relevances[layer+1]/z).data                                     
+            s = (relevances[layer+1]/z).data
             # Calculate the gradient and multiply it by the activation
-            (z * s).sum().backward(); 
-            c = activations[layer].grad       
-            # Assign new relevance values           
-            relevances[layer] = (activations[layer]*c).data                          
+            (z * s).sum().backward()
+            c = activations[layer].grad
+            # Assign new relevance values
+            relevances[layer] = (activations[layer]*c).data
         else:
             relevances[layer] = relevances[layer+1]
 
     # >>> Potential Step 5: Apply different propagation rule for pixels
     return relevances[0]
 
+
 # %%
 # Calculate relevances for first image in this test batch
 image_id = 31
 image_relevances = apply_lrp_on_vgg16(model, inputs[image_id])
-image_relevances = image_relevances.permute(0,2,3,1).detach().cpu().numpy()[0]
+image_relevances = image_relevances.permute(
+    0, 2, 3, 1).detach().cpu().numpy()[0]
 image_relevances = np.interp(image_relevances, (image_relevances.min(),
-                                                image_relevances.max()), 
-                                                (0, 1))
+                                                image_relevances.max()),
+                             (0, 1))
 # Show relevances
 pred_label = list(test_dataset.class_to_idx.keys())[
-             list(test_dataset.class_to_idx.values())
-            .index(labels[image_id])]
+    list(test_dataset.class_to_idx.values())
+    .index(labels[image_id])]
 if outputs[image_id] == labels[image_id]:
     print("Groundtruth for this image: ", pred_label)
 
     # Plot images next to each other
     plt.axis('off')
-    plt.subplot(1,2,1)
-    plt.imshow(image_relevances[:,:,0], cmap="seismic")
-    plt.subplot(1,2,2)
-    plt.imshow(inputs[image_id].permute(1,2,0).detach().cpu().numpy())
+    plt.subplot(1, 2, 1)
+    plt.imshow(image_relevances[:, :, 0], cmap="seismic")
+    plt.subplot(1, 2, 2)
+    plt.imshow(inputs[image_id].permute(1, 2, 0).detach().cpu().numpy())
     plt.show()
 else:
     print("This image is not classified correctly.")
